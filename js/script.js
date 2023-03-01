bfa7060447f5aff59fbd07a69af1fa533079564d
Aranel9966 <99angelo.dire@gmail.com>
2023-03-01 21:07:06 +0100

    add event and style

--- js/script.js
+++ js/script.js
@@ -9,14 +9,38 @@ Data una lista della spesa (memorizzata in un array),
 /*
 -creo array
 -ciclo while per stampare 
+-creo un evento per aggiungere elementi nella lista
 */
 
 const shoppingList =['carote','patate','pomodori','cipolle','finocchi','zucchine'];
-
+let addEl =document.getElementById('add');
+let listEl=document.getElementById('list');
+let containerEl=document.getElementById('container');
 let index=0;
 
+containerEl.style.fontSize='30px'
+containerEl.style.backgroundColor='#00ff00'
+
 while(shoppingList.length != index){
-    document.writeln(shoppingList[index] + '<br>')
+    
+    let stampa = document.createElement('li');
+
+    stampa.innerHTML=(shoppingList[index])
+
+    listEl.append(stampa);
+
+
     console.log(shoppingList[index])
     index++
 }
+
+addEl.addEventListener('click',function(){
+    let newAdd = prompt('cosa vuoi aggiungere?')
+
+    let stampa = document.createElement('li');
+
+    stampa.innerHTML=(newAdd)
+
+    listEl.append(stampa);
+    
+})
